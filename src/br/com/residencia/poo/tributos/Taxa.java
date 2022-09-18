cae8f459ca9e3e5191f008908e92b0acbc4b679d
nello0517 <aniellolongo555@gmail.com>
2022-09-18 06:35:56 -0300

    Anderson Andrade, Aniello Longobardi, Gabriel Mendes, Quezia Menezes e Sulamita Rodrigues

--- src/br/com/residencia/poo/tributos/Taxa.java
+++
@@ -1,8 +0,0 @@
-package br.com.residencia.poo.tributos;
-
-public interface Taxa {
-	double SEGURODEVIDA = 0.2, RENDIMENTO = 0.2;
-
-	// double taxaSeguroDeVida(double valor);
-	// double taxaRendimento (double valor);
-}
